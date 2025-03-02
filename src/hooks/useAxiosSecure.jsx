14f15fab87a48dc9e86dadff2fbda51f43d3ec99
subahan ali <subahanislam523@gmail.com>
2025-03-02 16:10:22 +0600

    added to by to custom hook useaxiossecure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,28 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 const useAxiosSecure = () => {
+  // axios interceptor
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
+  axiosSecure.interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    async (error) => {
+      console.log("Error from to interceptor", error.response);
+      if (error.response.status === 401 || error.response.status === 403) {
+        await logOut();
+        navigate("/login");
+      }
+      return Promise.reject.error;
+    }
+  );
   return axiosSecure;
 };
 
